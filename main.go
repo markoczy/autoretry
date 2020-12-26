a0007752aefcb1d89b2b7078277edb49d9405119
Aleistar Markóczy <a.markoczy@gmail.com>
2020-12-26 13:13:31 +0100

    Max retries

--- main.go
+++ main.go
@@ -12,12 +12,16 @@ import (
 func main() {
 	timeoutFlag := flag.Duration("timeout", 1*time.Hour, "timeout for command retry")
 	nologFlag := flag.Bool("nolog", false, "disable all logging")
+	maxFlag := flag.Int("max", -1, "max retries '-1' for infinite")
 	flag.Parse()
 
 	timeout := *timeoutFlag
 	nolog := *nologFlag
+	max := *maxFlag
 	args := flag.Args()
-	for {
+
+	retries := 0
+	for max != -1 && retries < max {
 		ctx, cancel := context.WithTimeout(context.Background(), timeout)
 		defer cancel()
 		out, err := exec.CommandContext(ctx, args[0], args[1:]...).Output()
@@ -28,5 +32,6 @@ func main() {
 		if !nolog {
 			log.Println("Autoretry - ERROR:", err)
 		}
+		retries++
 	}
 }
